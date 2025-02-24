afd1770f5bb75509d9889e1bcb6b1b3f900e9518
SupriyaRana2003 <156180067+SupriyaRana2003@users.noreply.github.com>
2025-02-24 18:31:19 +0000

    container

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 234561;
+let accountEmail = "supriya123@gmail.com";
+var accountPassword = "23456";
+accountCity = "jaipur";
+let accountState;
+
+console.log(accountId);
+accountEmail = "fgdgd@gmail.com";
+accountPassword = "3456";
+accountCity ="Bangaluru";
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
+
+/*
+Prefer not to user var 
+because of issue in block scope  and functional scope
+*/
+
+
