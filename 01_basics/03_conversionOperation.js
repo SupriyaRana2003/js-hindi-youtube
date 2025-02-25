680414e7ff382e6307c77ceca497baef2c596948
SupriyaRana2003 <156180067+SupriyaRana2003@users.noreply.github.com>
2025-02-25 03:59:56 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,18 +12,37 @@
 //"33abc" =>NaN
 //true =>1; false =>0
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 =>true;
 //0 =>false;
 //"hitesh" >=true;
 //""=> false
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************Operation***********
+let value =3;
+let negValue = -value;
+console.log(negValue);
+let str1 = "hello";
+let str2 = "supriya";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(12+23 +"4");
+
+
+
+
 
 
