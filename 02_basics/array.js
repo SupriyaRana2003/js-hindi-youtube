12494874ffce8593d13c9b77a7b11ba594e615bc
SupriyaRana2003 <156180067+SupriyaRana2003@users.noreply.github.com>
2025-02-28 06:51:55 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,45 @@
+const myArr = [1,2,3,4,5,6,7];
+// const newArr= new Array(1,2,3,4);
+//method
+
+// myArr.push(9);
+// myArr.pop(9);
+// myArr.unshift(10);
+// myArr.shift();
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// const newArr= myArr.join();
+
+// console.log(myArr);
+// console.log(newArr);
+// ;
+
+// const myn1= myArr.slice(1,3);
+// console.log("A",myArr);
+// console.log(myn1);
+// const myn2 = myArr.splice(1,5);
+// console.log( "B",myArr);
+
+// console.log(myn2);
+const another_array =[1,3,4,5,6,[2,3,4],[5,6,7,[6,7,8,90]]]
+const real_another_array = another_array.flat(2);
+console.log(real_another_array);
+// console.log(Array.isArray("Supriya"));
+// console.log(Array.from("Supriya"));
+// console.log(Array.from({name:"supriya"}));
+
+
+
+// let score1 =100;
+// let score2 = 200;
+// let score3 = 300;
+
+// console.log(Array.of(score1,score2,score3));
+// console.log(Array.of(score1,score2));
+
+
+
+
+
+
+
